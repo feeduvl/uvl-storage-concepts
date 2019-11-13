ab2c666f9c5ddb7912fdb330715ab7f531e56b3b
Christoph Stanik <stanik@informatik.uni-hamburg.de>
2019-11-13 12:29:52 +0100

    Updated the mask name of numbers to NUMBERS to better represent the data as the previous label (PHONE) did not cover all cases

--- anonymization_test.go
+++ anonymization_test.go
@@ -3,12 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 var testStrIn = "Leo leo Leonie 12 123 1234 12345 123456 1234567 #1234567890 nAbCdEf01G23h456I And some more non-matching @content"
-var testStrOut = "[PERSON] [PERSON] Leonie 12 [PHONE] [PHONE] 12345 123456 [PHONE] [PHONE] [FISCALCODE] And some more non-matching @content"
+var testStrOut = "[PERSON] [PERSON] Leonie 12 [NUMBER] [NUMBER] 12345 123456 [NUMBER] [NUMBER] [FISCALCODE] And some more non-matching @content"
 
 func TestAnonymizeString(t *testing.T) {
 	assert.Equal(t, AnonymizeString(testStrIn), testStrOut)
