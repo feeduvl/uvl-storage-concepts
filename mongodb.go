a45b2df4f8e0364c1ee1fafcdc28917434df87fd
Michael Anders <M.Anders@stud.uni-heidelberg.de>
2022-08-02 22:06:39 +0200

    Revert "update added"
    This reverts commit dfd5ee5dad3dd71c9f95fc4f9b1ae95d85e73438.

--- mongodb.go
+++ mongodb.go
@@ -452,11 +452,3 @@ func MongoDeleteCrawlerJob(mongoClient *mgo.Session, date time.Time) error {
 
 	return err
 }
-
-func MongoUpdateCrawlerJob(mongoClient *mgo.Session, date time.Time) error {
-	query := bson.M(fieldCrawlerJobDate: date)
-	update := bson.M{"$set": bson.M{"occurrence": 0}}
-	_, err := mongoClient.DB(database).C(collectionCrawlerJobs).Update(query, update)
-
-	return err
-}
