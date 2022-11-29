66416846dc7e1ae4b4dd43fdf564bc6c0cc43ff4
Anh Tu Duong Nguyen <anhtuduong.nguyen@outlook.de>
2022-11-29 14:57:36 +0100

    Revert "changed input to AppReviewCrawlerJobs"
    This reverts commit b9d916bf2be7401284b4f81ff655b39e30673df3.

--- starter.go
+++ starter.go
@@ -763,7 +763,7 @@ func postAppReviewCrawlerJobs(w http.ResponseWriter, r *http.Request) {
 		panic(err) 
 	}
 
-	err = json.Unmarshal(s, &AppReviewCrawlerJobs)
+	err = json.Unmarshal(s, &crawlerJobs)
 	if err != nil {
 		fmt.Printf("ERROR decoding json: %s for request body: %v\n", err, r.Body)
 		w.WriteHeader(http.StatusBadRequest)
@@ -772,7 +772,7 @@ func postAppReviewCrawlerJobs(w http.ResponseWriter, r *http.Request) {
 
 	m := mongoClient.Copy()
 	defer m.Close()
-	err = MongoInsertAppReviewCrawlerJobs(m, CrawlerJobs)
+	err = MongoInsertAppReviewCrawlerJobs(m, crawlerJobs)
 	if err != nil {
 		fmt.Printf("ERROR %s\n", err)
 		w.WriteHeader(http.StatusBadRequest)
