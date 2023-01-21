4d64991db19811722909da8ed996748ab043bc82
Anh Tu Duong Nguyen <anhtuduong.nguyen@outlook.de>
2023-01-21 15:36:42 +0100

    enable to search for app_name

--- mongodb.go
+++ mongodb.go
@@ -482,7 +482,7 @@ func MongoGetAppReviewCrawlerJobs(mongoClient *mgo.Session) [] AppReviewCrawlerJ
 
 	err := mongoClient.
 		DB(database).
-		C(collectionAppReviewCrawlerJobs).Find(bson.M{}).Select(bson.M{"app_url": 1, "date": 1, "occurrence": 1, "number_posts": 1, "dataset_name": 1, "request": 1}).All(&crawlerJobs)
+		C(collectionAppReviewCrawlerJobs).Find(bson.M{}).Select(bson.M{"app_name": 1, "app_url": 1, "date": 1, "occurrence": 1, "number_posts": 1, "dataset_name": 1, "request": 1}).All(&crawlerJobs)
 	
 	if err != nil {
 		fmt.Println("ERR", err)
